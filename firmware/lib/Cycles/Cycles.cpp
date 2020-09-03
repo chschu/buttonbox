712c35d9aa46f9e94365cd2f57b0eec66c45de13
Christian Schuster <blackyle@me.com>
2020-09-03 22:10:52 +0200

    restore interrupt flag as soon as possible

--- firmware/lib/Cycles/Cycles.cpp
+++ firmware/lib/Cycles/Cycles.cpp
@@ -23,20 +23,21 @@ void cycles_init() {
 uint32_t cycles_get() {
     uint8_t prevSREG = SREG;
 
-    // disable interrupts for a consistent read of _cycles and TCNT1
+    // disable interrupts for a consistent read of _cycles_high and TCNT1
     cli();
 
-    // determine low and high word of cycle count
+    // determine low and high word of cycle count, as well as timer interrupt flags
     uint16_t cycles_low = TCNT1;
     uint16_t cycles_high = _cycles_high;
-
-    // check if there is an unhandled timer overflow and TCNT1 had already wrapped around before it was read
-    if ((TIFR1 & _BV(TOV1)) && cycles_low < 0x8000) {
-        cycles_high++;
-    }
+    uint16_t flags = TIFR1;
 
     // restore interrupt flag
     SREG = prevSREG;
+
+    // check if there is a pending timer overflow and TCNT1 had already wrapped around before it was read
+    if ((flags & _BV(TOV1)) && cycles_low < 0x8000) {
+        cycles_high++;
+    }
 
     return ((uint32_t) cycles_high << 16) | cycles_low;
 }
